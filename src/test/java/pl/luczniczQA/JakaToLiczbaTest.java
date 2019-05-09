1b9ffb96b58f99431daf4fcc4af50af382addc14
ciecierski <19051965!QAZ2wsx>
2019-05-09 19:23:07 +0200

    Implementacja testu dla ujemnych wartości nieparzystych. Wykonanie testów. Raportowanie. Wyniki negatywne. Klasa testowana wymaga implementacji.

--- src/test/java/pl/luczniczQA/JakaToLiczbaTest.java
+++ src/test/java/pl/luczniczQA/JakaToLiczbaTest.java
@@ -16,26 +16,55 @@ class JakaToLiczbaTest {
     final private int LICZBA_MINUS_JEDEN =-1;
     final private int LICZBA_MINUS_DWA =-2;
     final private int LICZBA_MINIMALNA_UJEMNA =Integer.MIN_VALUE; //-2147483648
+    final private String LICZBA_UJEMNA_NIEPARZYSTA_KOMUNIKAT ="Nieparzysta liczba mniejsza od zera";
+
 
     final private int LICZBA_JEDEN =1;
     final private int LICZBA_DWA =2;
 
-    final private int LICZBA_DOWOLNA_NIEPARZYSTA_UJEMNA =-3;
+    final private int LICZBA_DOWOLNA_NIEPARZYSTA_UJEMNA =-33;
     final private int LICZBA_DOWOLNA_NIEPARZYSTA_DODATNIA =35;
     final private int LICZBA_MAKSYMALNA_DODATNIA =Integer.MAX_VALUE; //2147483647
 
     final private int LICZBA_DOWOLNA_PARZYSTA_UJEMNA =-1880;
     final private int LICZBA_DOWOLNA_PARZYSTA_DODATNIA =550;
 
-final private String TEST_OK="_test ok_";
+final private String TEST_OK="_test ok_ dla wartości: ";
 
+    /**
+     * testowana metoda powinna zwrócić pożądany komunikat
+     */
     @Test
     void powinnoBycZero() {
-        /**
-         * testowana metoda powinna zwrócić pożądany komunikat
-         */
+
         JakaToLiczba liczba = new JakaToLiczba();
-        liczba.jakaToLiczba(LICZBA_ZERO);
-        assertTrue(liczba.jakaToLiczba(LICZBA_ZERO)==LICZBA_ZERO_KOMUNIKAT, TEST_OK);
+
+        assertTrue(liczba.jakaToLiczba(LICZBA_ZERO)==LICZBA_ZERO_KOMUNIKAT, TEST_OK+LICZBA_ZERO);
     }
+
+
+    @Test
+    void powinnaBycLiczbaUjemnaNieparzystaMinusJeden() {
+
+        JakaToLiczba liczba = new JakaToLiczba();
+
+        assertTrue(liczba.jakaToLiczba(LICZBA_MINUS_JEDEN)==LICZBA_UJEMNA_NIEPARZYSTA_KOMUNIKAT, TEST_OK+ LICZBA_MINUS_JEDEN);
+    }
+
+    @Test
+    void powinnaBycLiczbaUjemnaNieparzystaMinimalna() {
+
+        JakaToLiczba liczba = new JakaToLiczba();
+
+        assertTrue(liczba.jakaToLiczba(LICZBA_MINIMALNA_UJEMNA)==LICZBA_UJEMNA_NIEPARZYSTA_KOMUNIKAT, TEST_OK+ LICZBA_MINIMALNA_UJEMNA);
+    }
+
+    @Test
+    void powinnaBycLiczbaUjemnaNieparzystaDowolna() {
+
+        JakaToLiczba liczba = new JakaToLiczba();
+
+        assertTrue(liczba.jakaToLiczba(LICZBA_DOWOLNA_NIEPARZYSTA_UJEMNA)==LICZBA_UJEMNA_NIEPARZYSTA_KOMUNIKAT, TEST_OK+ LICZBA_DOWOLNA_NIEPARZYSTA_UJEMNA);
+    }
+
 }
