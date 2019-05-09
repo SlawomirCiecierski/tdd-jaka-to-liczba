c65d071e438db825d2352cafe8b61a0eb5470452
ciecierski <19051965!QAZ2wsx>
2019-05-09 21:31:50 +0200

    Refactoring klasy testowanej: zaimplementowanie prywatnej metody sprawdzającej parzystość. Wykonanie testów -> OK.

--- src/main/java/pl/luczniczQA/JakaToLiczba.java
+++ src/main/java/pl/luczniczQA/JakaToLiczba.java
@@ -2,18 +2,19 @@ package pl.luczniczQA;
 
 public class JakaToLiczba {
 
+
+    private boolean parzysta(int liczba) {
+        if (liczba % 2 == 0) return true;
+        else return false;
+    }
+
     String jakaToLiczba(int liczba) {
 
         if (liczba == 0) return "To jest 0";
-        else if (liczba < 0 && liczba % 2 != 0) return "Nieparzysta liczba mniejsza od zera";
-        else if (liczba < 0 && liczba % 2 == 0) return "Parzysta liczba mniejsza od zera";
-        else if (liczba > 0 && liczba % 2 != 0) return "Nieparzysta liczba większa od zera";
-        else if (liczba > 0 && liczba % 2 == 0) return "Parzysta liczba większa od zera";
-
-
-
-        else {
-            return "Błąd";
-        }
+        else if (liczba < 0 && !parzysta(liczba)) return "Nieparzysta liczba mniejsza od zera";
+        else if (liczba < 0 && parzysta(liczba)) return "Parzysta liczba mniejsza od zera";
+        else if (liczba > 0 && !parzysta(liczba)) return "Nieparzysta liczba większa od zera";
+        else if (liczba > 0 && parzysta(liczba)) return "Parzysta liczba większa od zera";
+        else return "Błąd";
     }
 }
